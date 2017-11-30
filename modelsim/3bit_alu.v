1c26d5ba93abb44970622950b45b3cf3ccb29ade
cmacsemchuk <macsemchuk@gmail.com>
2017-11-29 22:49:21 -0500

    ALU module added

---
+++ modelsim/3bit_alu.v
@@ -0,0 +1,70 @@
+//-------------------------------------------------------------
+// ENGI 4054: Digital VLSI Design
+// Author: Craig Macsemchuk & Natalie Marcinkowski
+//
+// Filename: 3bit_alu.v
+// Date: Nov. 27th, 2017
+// Description: **TODO
+//
+// Inputs: 
+// Outputs: 
+//-------------------------------------------------------------
+
+module three_bit_alu(
+   input [2:0]a,
+   input [2:0]b,
+   input [1:0]op_code,
+   input rst_n,
+   input bbclk,
+   output reg [5:0]op_out,
+   output reg carry_out
+);
+
+
+wire [5:0]bw_and_out;
+wire [5:0]bw_or_out;
+wire [5:0]add_out;
+wire [5:0]multi_out;
+wire carry;
+
+
+           //dff_sync sync_a0(rst_n,bbclk,a[0],a_d[0]);
+          // dff_sync sync_a1(rst_n,bbclk,a[1],a_d[1]);
+          // dff_sync sync_a2(rst_n,bbclk,a[2],a_d[2]);
+           //dff_sync sync_b0(rst_n,bbclk,b[0],b_d[0]);
+           //dff_sync sync_b1(rst_n,bbclk,b[1],b_d[1]);
+           //dff_sync sync_b2(rst_n,bbclk,b[2],b_d[2]);
+
+           //dff_sync sync_op1(rst_n,bbclk,b[2],b_d[2]);
+           //dff_sync sync_op2(rst_n,bbclk,b[2],b_d[2]);
+
+   bitwise_and u_one(a,b,bw_and_out);
+   bitwise_or u_two(a,b,bw_or_out);
+   three_bit_full_adder_core u_three(a,b,add_out,carry);
+   three_bit_multiplier u_four(a,b,multi_out);
+
+always @ (posedge bbclk or negedge rst_n)
+   begin
+      if (!rst_n)
+         begin
+
+         op_out <= 6'b000000;
+
+         carry_out <= 1'b0;
+        end 
+     else     
+   begin
+   case(op_code)
+   2'b00:op_out = bw_and_out;
+   2'b01:op_out <= bw_or_out;
+   2'b10:begin
+         op_out <= add_out;
+         carry_out <= carry;
+         end
+   2'b11:op_out <= multi_out;
+   endcase
+ end
+        end
+   
+
+endmodule 
--- modelsim/3bit_multiplier.v
+++ modelsim/3bit_multiplier.v
@@ -10,26 +10,22 @@
 // Outputs: 
 //          
 //-------------------------------------------------------------
-/*
-module full_adder_core(
-   input in_a, // augend
-   input in_b, // addend
-   input in_c, // carry-in flag
-   output s_out, //summation out
-   output c_out //summation carry out
-*/
 module three_bit_multiplier(
    input [2:0]a,
    input [2:0]b,
    output [5:0]product_out //bitwise AND out
 );
-   //Wires 
-   //bit one
-   wire a0_and_b0_w;
+//----------WIRE/REG---------------------
+//---------------------------------------
+// BIT ONE
+//---------------------------------------
+   wire a0_and_b1_w;
    wire a1_and_b0_w;
    wire carry_one_w;
    
-   //bit two
+//---------------------------------------
+// BIT TWO
+//---------------------------------------
    wire a2_and_b0_w;
    wire a1_and_b1_w;
    wire a0_and_b2_w;
@@ -37,7 +33,9 @@ module three_bit_multiplier(
    wire carry_two_half_w;
    wire full_two_out_w;
    
-   //bit three
+//---------------------------------------
+// BIT THREE
+//---------------------------------------
    wire a2_and_b1_w;
    wire a1_and_b2_w;
   
@@ -45,35 +43,50 @@ module three_bit_multiplier(
    wire carry_three_half_w;
    wire half_three_out_w; 
 
-   //bit four
+//---------------------------------------
+// BIT FOUR
+//---------------------------------------
    wire a2_and_b2_w; 
 
-   // Bit zero
-   and and_a0_b1(product_out[0], a[0],b[0]);
+//-----------MODULE-INSTANTIATE-----------
+//---------------------------------------
+// BIT ZERO
+//---------------------------------------
+   and and_a0_b0(product_out[0], a[0],b[0]);
 
-   // Bit one
-   and and_a0_b0(a0_and_b0_w, a[0], b[0]);
+//---------------------------------------
+// BIT ONE
+//---------------------------------------
+   and and_a0_b1(a0_and_b1_w, a[0], b[1]);
    and and_a1_b0(a1_and_b0_w, a[1], b[0]);
-   half_adder_core ha_one(a0_and_b0_w,a1_and_b0_w,product_out[1],carry_one_wire);
+   
+   half_adder_core ha_one(a0_and_b1_w,a1_and_b0_w,product_out[1],carry_one_w);
 
-   // Bit two
+//---------------------------------------
+// BIT TWO
+//---------------------------------------
    and and_a2_b0(a2_and_b0_w, a[2], b[0]);
    and and_a1_b1(a1_and_b1_w, a[1], b[1]);
    and and_a0_b2(a0_and_b2_w, a[0], b[2]);
-   full_adder_core fa_two(a0_and_b0_w,a1_and_b0_w,carry_one_w,stage_one_out_w,carry_two_full_w);
+   
+   full_adder_core fa_two(a1_and_b1_w,a2_and_b0_w,carry_one_w,full_two_out_w,carry_two_full_w);
    half_adder_core ha_two(a0_and_b2_w,full_two_out_w,product_out[2],carry_two_half_w);
    
-   // Bit three
+//---------------------------------------
+// BIT THREE
+//---------------------------------------
    and and_a2_b1(a2_and_b1_w, a[2], b[1]);
    and and_a1_b2(a1_and_b2_w, a[1], b[2]);
-   half_adder_core ha_three(carry_two_full,a2_and_b1_w,half_three_out_w,carry_three_half_w);
-   full_adder_core fa_three(half_three_out,a1_and_b2_w,carry_two_half_w,product_out[3],carry_three_full_w);
+   
+   half_adder_core ha_three(carry_two_full_w,a2_and_b1_w,half_three_out_w,carry_three_half_w);
+   full_adder_core fa_three(half_three_out_w,a1_and_b2_w,carry_two_half_w,product_out[3],carry_three_full_w);
 
-   // Bit four
+//---------------------------------------
+// BIT FOUR
+//---------------------------------------
    and and_a2_b2(a2_and_b2_w, a[2], b[2]);
-   full_adder_core fa_four(carry_three_half_w,a1_and_b2_w,carry_three_full_w,product_out[4],product_out[5]);
-
-
+   
+   full_adder_core fa_four(carry_three_half_w,a2_and_b2_w,carry_three_full_w,product_out[4],product_out[5]);
 
 
 endmodule
